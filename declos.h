314b4d12c3efc80c467fe6848727bc6e874b2c17
AnqurVanillapy <anqurvanillapy@gmail.com>
2018-06-10 13:50:56 +0800

    fmt

--- declos.h
+++ declos.h
@@ -5,17 +5,15 @@
 
 typedef jmp_buf clos_t;
 
-#define declos(name, body) \
-	clos_t __clos_in_##name; \
-	clos_t __clos_out_##name; \
-	if (setjmp(__clos_in_##name)) { \
-		body; \
-		longjmp(__clos_out_##name, 1); \
+#define declos(name, body)              \
+	clos_t __clos_in_##name;            \
+	clos_t __clos_out_##name;           \
+	if (setjmp(__clos_in_##name)) {     \
+		body;                           \
+		longjmp(__clos_out_##name, 1);  \
 	}
 
 #define clos_run(name) \
-	if (!setjmp(__clos_out_##name)) { \
-		longjmp(__clos_in_##name, 1); \
-	}
+	if (!setjmp(__clos_out_##name)) longjmp(__clos_in_##name, 1);
 
-#endif /* _DECLOS_H */
+#endif /* !_DECLOS_H */
